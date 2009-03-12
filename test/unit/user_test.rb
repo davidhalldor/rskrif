82113f43604844e01c069e6f540ae3d21bc99833
Davíð Lúðvíksson <davidhalldor@gmail.com>
2009-03-12 20:15:22 +0000

    Added unit tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,23 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  fixtures :users
+
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_create_valid_user
+    user = User.new(:name => 'siggi', :email => 'siggi@example.com',
+            :password => 'abc123', :password_confirmation => 'abc123')
+    assert user.save
   end
+
+  test "invalid with empty attributes" do
+    user = User.new
+    assert !user.valid?
+    assert user.errors.invalid?(:name)
+    assert user.errors.invalid?(:email)
+    assert user.errors.invalid?(:password)
+    assert user.errors.invalid?(:password_confirmation)
+  end
+
 end
